8475de55d442f2d2de03192c63c81cdd4ca4d63f
EricHorton <ewhorton@ncsu.edu>
2017-11-09 12:26:31 -0500

    Merging into snippets directory.

--- eric/3437059.py
+++
@@ -1,5 +0,0 @@
-s = "This be a string"
-if s.find("is") == -1:
-    print "No 'is' here!"
-else:
-    print "Found 'is' in the string."
--- eric/466345.py
+++
@@ -1,3 +0,0 @@
-from datetime import datetime
-
-datetime_object = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
